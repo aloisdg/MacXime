d32a103c5015bfc8efed35906b79c945a4b9c1c6
alois <aloisdegouvello@live.fr>
2015-04-16 15:51:39 +0200

    Add core lib

---
+++ MacXime/ProcessorHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacXime
+{
+	// http://stackoverflow.com/a/336729/1248177
+
+	public static class ProcessorHelper
+	{
+		static readonly bool Is64BitProcess = (IntPtr.Size == 8);
+		static bool _is64BitOperatingSystem = Is64BitProcess || InternalCheckIsWow64();
+
+		[DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool IsWow64Process(
+		    [In] IntPtr hProcess,
+		    [Out] out bool wow64Process
+		);
+
+		public static bool InternalCheckIsWow64()
+		{
+			if ((Environment.OSVersion.Version.Major != 5 || Environment.OSVersion.Version.Minor < 1)
+				&& Environment.OSVersion.Version.Major < 6) return false;
+			using (var p = Process.GetCurrentProcess())
+			{
+				bool retVal;
+				return IsWow64Process(p.Handle, out retVal) && retVal;
+			}
+		}
+	}
+}
---
+++ MacXime/Spoofer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace MacXime
+{
+	// http://www.windows-commandline.com/get-mac-address-command-line/
+
+	// http://www.irongeek.com/i.php?page=security/madmacs-mac-spoofer
+	// http://stackoverflow.com/questions/22310464/how-to-spoof-mac-address-via-c-sharp-code
+	// http://www.codeproject.com/Articles/10493/MAC-Address-Changer-for-Windows-XP
+
+	public class Spoofer
+	{
+		private const string BaseReg = @"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\";
+
+		public static bool SetMac(string nicid, string newmac)
+		{
+			var ret = false;
+			using (var bkey = GetBaseKey())
+			using (var key = bkey.OpenSubKey(BaseReg + nicid))
+			{
+				if (key == null) return false;
+				key.SetValue("NetworkAddress", newmac, RegistryValueKind.String);
+
+				var mos = new ManagementObjectSearcher(
+					new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE Index = " + nicid));
+
+				foreach (var o in mos.Get().OfType<ManagementObject>())
+				{
+					o.InvokeMethod("Disable", null);
+					o.InvokeMethod("Enable", null);
+					ret = true;
+				}
+			}
+			return ret;
+		}
+
+		public static IEnumerable<string> GetNicIds()
+		{
+			using (var bkey = GetBaseKey())
+			using (var key = bkey.OpenSubKey(BaseReg))
+			{
+				if (key == null) yield break;
+				foreach (var name in key.GetSubKeyNames().Where(n => !n.Equals("Properties")))
+				{
+					using (var sub = key.OpenSubKey(name))
+					{
+						if (sub != null)
+						{
+							var busType = sub.GetValue("BusType");
+							var busStr = busType != null ? busType.ToString() : String.Empty;
+							if (!String.IsNullOrEmpty(busStr))
+								yield return name;
+
+							//var busType = sub.GetValue("BusType");
+							//if (busType != null)
+							//	yield return name;
+						}
+					}
+				}
+			}
+		}
+
+		public static RegistryKey GetBaseKey()
+		{
+			return RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,
+			    //ProcessorHelper.InternalCheckIsWow64()
+			    Environment.Is64BitOperatingSystem
+			    ? RegistryView.Registry64 : RegistryView.Registry32);
+		}
+	}
+}
