612146854095acc918993f195a13cfc38f53bed9
alois <aloisdegouvello@live.fr>
2015-04-16 16:12:11 +0200

    move code

--- MacXime/Spoofer.cs
+++ MacXime/Spoofer.cs
@@ -18,28 +18,6 @@ namespace MacXime
 	{
 		private const string BaseReg = @"SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}\";
 
-		public static bool SetMac(string nicid, string newmac)
-		{
-			var ret = false;
-			using (var bkey = GetBaseKey())
-			using (var key = bkey.OpenSubKey(BaseReg + nicid))
-			{
-				if (key == null) return false;
-				key.SetValue("NetworkAddress", newmac, RegistryValueKind.String);
-
-				var mos = new ManagementObjectSearcher(
-					new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE Index = " + nicid));
-
-				foreach (var o in mos.Get().OfType<ManagementObject>())
-				{
-					o.InvokeMethod("Disable", null);
-					o.InvokeMethod("Enable", null);
-					ret = true;
-				}
-			}
-			return ret;
-		}
-
 		public static IEnumerable<string> GetNicIds()
 		{
 			using (var bkey = GetBaseKey())
@@ -66,20 +44,42 @@ namespace MacXime
 			}
 		}
 
-		public static RegistryKey GetBaseKey()
+		public static bool SetMac(string nicid, string newmac = null)
+		{
+			newmac = newmac ?? GenerateMacAddress();
+			var ret = false;
+			using (var bkey = GetBaseKey())
+			using (var key = bkey.OpenSubKey(BaseReg + nicid))
+			{
+				if (key == null) return false;
+				key.SetValue("NetworkAddress", newmac, RegistryValueKind.String);
+
+				var mos = new ManagementObjectSearcher(
+					new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE Index = " + nicid));
+
+				foreach (var o in mos.Get().OfType<ManagementObject>())
+				{
+					o.InvokeMethod("Disable", null);
+					o.InvokeMethod("Enable", null);
+					ret = true;
+				}
+			}
+			return ret;
+		}
+
+		private static RegistryKey GetBaseKey()
 		{
 			return RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,
 			    Environment.Is64BitOperatingSystem
 			    ? RegistryView.Registry64 : RegistryView.Registry32);
 		}
 
-		public static string GenerateMacAddress()
+		private static string GenerateMacAddress()
 		{
 			var random = new Random();
 			var buffer = new byte[6];
 			random.NextBytes(buffer);
-			var result = String.Concat(buffer.Select(x => x.ToString("X2") + ':'));
-			return result.TrimEnd(':');
+			return String.Concat(buffer.Select(x => x.ToString("X2") + ':')).TrimEnd(':');
 		}
 	}
 }
